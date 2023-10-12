690f2a3913af7a7fbe1c5eab1f9f1648507eb6ec
Yuliya <iksulia1@gmail.com>
2023-10-12 21:28:50 +0300

    change

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -30,7 +30,7 @@ export class UsersController {
 
     @ApiOkResponse({type: LoginCheckResponse})
     @Get('/login-check')
-    //@UseGuards(AuthenticatedGuard)
+    @UseGuards(AuthenticatedGuard)
     loginCheck(@Request() req){
         return req.user;
     }
