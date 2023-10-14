7958d18dc64db99545cb8c235c1d56c77b8f0326
Yuliya <iksulia1@gmail.com>
2023-10-14 14:19:50 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
 
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3001, https://boiler-mart-client.vercel.app']
+    origin: ['*']
   });
 
   const config = new DocumentBuilder()
