88ff3cce4cea2aa2cfead56420e7173f6a55313a
Yuliya <iksulia1@gmail.com>
2023-10-14 17:58:04 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -15,11 +15,17 @@ async function bootstrap() {
   app.use(passport.initialize());
   app.use(passport.session());
 
-  
-  app.enableCors({
-    credentials: true,
-    origin: ['https://tubular-cobbler-ec8367.netlify.app']
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://tubular-cobbler-ec8367.netlify.app'); 
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+    next();
   });
+  // app.enableCors({
+  //   credentials: true,
+  //   origin: ['https://tubular-cobbler-ec8367.netlify.app']
+  // });
 
   const config = new DocumentBuilder()
   .setTitle('BoilerMart')
